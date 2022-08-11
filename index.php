b7972d60712bc82993e6548932a6a5ad98abbc9a
AhmedBanafa <108236137+AhmedBanafa@users.noreply.github.com>
2022-08-11 22:11:10 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+$forward =  $_GET['forward'];
+$backward =  $_GET['backward'];
+$left =  $_GET['left'];
+$right =  $_GET['right'];
+$stop =  $_GET['stop'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="index.php" method="get">
+    <input type="text" name="forward" id="forward">
+    <input type="text" name="backward" id="backward">
+    <input type="text" name="left" id="left">
+    <input type="text" name="right" id="right">
+    <input type="text" name="stop" id="stop">
+
+
+</form>
+
+<script src="./js/script.js"></script>
+</body>
+</html>
